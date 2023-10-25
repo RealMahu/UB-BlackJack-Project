1a5de93a215c4d44f7ce5cd27d7b70db7d370f7e
Michelle <m.straeszer@icloud.com>
2023-10-25 11:17:30 +0200

    blackjack_restructured

--- blackjack.js
+++ blackjack.js
@@ -1,147 +1,135 @@
-const readline = require("readline");
-// import readline from 'readline-sync';
+import inquirer from 'inquirer';
+ 
+const symbole = ["♥", "♠", "♦", "♣"];
+const werte = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Bube", "Dame", "König", "Ass"];
+const karten = [];
+ 
 
-class Card {
-	constructor(value, suit) {
-    this.value = value;
-    this.suit = suit;
-}
-
-cardValue() {
-    if (this.value > 1 || this.value < 11) {
-		return this.value;
+const spielerName = await inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Bitte gib deinen Namen ein:'
     }
-    if (this.value === "J" || this.value === "Q" || this.value === "K") {
-		return 10;
+]);
+console.log(`Willkommen beim Blackjack, ${spielerName.name}!\n`);
+ 
+function deckZuruecksetzen() {
+    karten.length = 0;
+ 
+    for (const symbol of symbole) {
+        for (const wert of werte) {
+            karten.push(`${wert} ${symbol}`);
+        }
     }
-    	return 11; // default for Ace
-	}
-}
-
-class Deck {
-  // create 52 cards
-constructor() {
-    let cards = [];
-    const suits = ["♥️", "♦️", "♣️", "♠️"];
-    for (let value = 2; value < 11; value++) {
-		for (const suit of suits) {
-			const card = new Card(value, suit);
-			cards.push(card);
-		}
-    }
-    const highCards = ["J", "Q", "K", "A"];
-    for (const highCard of highCards) {
-	for (const suit of suits) {
-        const card = new Card(highCard, suit);
-        cards.push(card);
-		}
-    }
-    console.log(cards);
-		return cards;
-	}
-}
-
-class BlackJack {
-	constructor() {
-    this.cards = new Deck();
-    this.score = 0;
-    console.log("Welcome to BlackJack.");
-    console.log(`The deck has ${this.cards.length} cards.`);
-}
-
-hasCards() {
-    return this.cards.length === 0;
-}
-
-hit() {
-    const card = this.cards[Math.floor(Math.random() * this.cards.length)];
-    const index = this.cards.indexOf(card);
-    this.cards.splice(index, 1);
-    console.log(
-	`${this.cards.length} cards left, ${card.value}${
-        card.suit
-	} was removed with value ${card.cardValue()}.`
-    );
-    const gamePlayStatus = this.checkScore(card);
-    return { card: card, gamePlayStatus: gamePlayStatus, score: this.score };
-}
-
-checkScore(card) {
-    this.score += card.cardValue();
-    console.log(this.score);
-    if (this.score === 21) {
-		return "blackJack";
-    } else if (this.score > 21) {
-		return "bust";
-    } else {
-		return "continue";
+ 
+    for (let i = karten.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [karten[i], karten[j]] = [karten[j], karten[i]];
     }
 }
-
-getScore() {
-    return this.score;
+ 
+function karteZiehen() {
+    const zufallsIndex = Math.floor(Math.random() * karten.length);
+    return karten.splice(zufallsIndex, 1)[0];
 }
-
-computerScore() {
-    const score = Math.floor(Math.random() * (21 - 18) + 18);
-    return score;
-}
-
-stand() {
-    const playerScore = blackJack.getScore();
-    const computerScore = blackJack.computerScore();
-    const playerScoreDiff = 21 - playerScore;
-    const computerScoreDiff = 21 - computerScore;
-    if (computerScore > 21) {
-		console.log(
-			`You won with ${playerScore} and the computer lost with ${computerScore}`
-		);
-		} else {
-			if (computerScoreDiff === playerScoreDiff) {
-				console.log(
-				`Tie!, computer score is ${computerScore} and your score is ${playerScore}`
-				);
-			} else if (computerScoreDiff < playerScoreDiff) {
-				console.log(
-				`You lost with ${playerScore} and the computer won with ${computerScore}`
-				);
-			} else {
-				console.log(
-				`You won with ${playerScore} and the computer lost with ${computerScore}`
-				);
-			}
-		}
-	}
-}
-
-//---->Game start
-
-const blackJack = new BlackJack();
-
-const r1 = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
-});
-
-const gamePlay = function () {
-r1.question("Do you want to `hit` or `stand`?", (answer) => {
-    if (answer === "hit") {
-		const result = blackJack.hit();
-		if (result.gamePlayStatus === "blackJack") {
-			console.log(`Bussin BlackJack!!!!`);
-			return r1.close();
-		}
-		if (result.gamePlayStatus === "bust") {
-			console.log(`Better luck next time bozo.`);
-			return r1.close();
-		}
+ 
+function handWert(hand) {
+    let wert = 0;
+    let asse = 0;
+    for (const karte of hand) {
+        const wort = karte.split(" ")[0];
+        if (wort === "Ass") {
+            wert += 11;
+            asse++;
+        } else if (wort === "König" || wort === "Dame" || wort === "Bube") {
+            wert += 10;
+        } else {
+            wert += parseInt(wort, 10);
+        }
     }
-    if (answer === "stand") {
-		blackJack.stand();
-		return r1.close();
+    while (wert > 21 && asse > 0) {
+        wert -= 10;
+        asse--;
     }
-		gamePlay();
-	});
-};
+    return wert;
+}
 
-gamePlay();
+function anzeigenHand(hand) {
+    process.stdout.write('\x1B[2J\x1B[H'); 
+    console.log(`Spielerhand: ${hand[0]}`);
+    console.log(`Dealerhand: ${hand[1]}`);
+}
+ 
+async function blackjackSpiel() {
+    let weiterspielen = true;
+ 
+    while (weiterspielen) {
+        deckZuruecksetzen();
+        const spielerHand = [karteZiehen(), karteZiehen()];
+        const dealerHand = [karteZiehen(), karteZiehen()];
+        let spielerWert = handWert(spielerHand);
+        let dealerWert = handWert(dealerHand);
+        anzeigenHand([spielerHand.join(", ") + ` (Gesamtwert: ${spielerWert})`, dealerHand[0] + ", ?"]);
+ 
+        while (spielerWert < 21) {
+            const zug = await inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'aktion',
+                    message: 'Möchtest du eine weitere Karte ziehen (Hit) oder Stehen bleiben (Stand)?',
+                    choices: ['Hit', 'Stand']
+                }
+            ]);
+ 
+            if (zug.aktion === 'Hit') {
+                const neueKarte = karteZiehen();
+                spielerHand.push(neueKarte);
+                spielerWert = handWert(spielerHand);
+                anzeigenHand([spielerHand.join(", ") + ` (Gesamtwert: ${spielerWert})`, dealerHand[0] + ", ?"]);
+            } else if (zug.aktion === 'Stand') {
+                break;
+            }
+        }
+ 
+        while (dealerWert < 17) {
+            const neueKarte = karteZiehen();
+            dealerHand.push(neueKarte);
+            dealerWert = handWert(dealerHand);
+        }
+        anzeigenHand([spielerHand.join(", ") + ` (Gesamtwert: ${spielerWert})`, dealerHand.join(", ") + ` (Gesamtwert: ${dealerWert})`]);
+ 
+        if (spielerWert === 21 && spielerHand.length === 2 && dealerWert !== 21) {
+            console.log(`\n${spielerName.name} hat Blackjack!`);
+        } else {
+        
+            if (spielerWert > 21 && dealerWert <= 21) {
+                console.log(`\n${spielerName.name} bust! Dealer gewinnt.`);
+            } else if (dealerWert > 21 && spielerWert <= 21) {
+                console.log(`\nDealer bust! ${spielerName.name} gewinnt.`);
+            } else if (spielerWert === dealerWert || (spielerWert > 21 && dealerWert > 21)) {
+                console.log("\nUnentschieden!");
+            } else if (spielerWert > dealerWert) {
+                console.log(`\n${spielerName.name}, du gewinnst mit ${spielerWert}.`);
+            } else {
+                console.log(`\nDealer gewinnt mit ${dealerWert}.`);
+            }
+        }
+ 
+        const antwort = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'weiter',
+                message: `Möchtest du eine weitere Runde spielen, ${spielerName.name}?`,
+                choices: ['Ja', 'Nein']
+            }
+        ]);
+        weiterspielen = antwort.weiter === 'Ja';
+        if (weiterspielen) {
+            console.clear();
+        }
+    }
+    console.log(`Danke fürs Spielen, ${spielerName.name}!`);
+}
+ 
+blackjackSpiel();
